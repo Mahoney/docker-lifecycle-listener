3de20aac6e8d7163717679a33529e8ccf1eaca07
Robert Elliot <rob@lidalia.org.uk>
2020-11-23 22:38:00 +0000

    Try and write a mac install script

--- install.sh
+++ install.sh
@@ -2,9 +2,20 @@
 
 set -euo pipefail
 
-docker build . -t docker-lifecycle-listener
+mkdir -p /usr/local/opt/docker-lifecycle-listener/sbin/
+cp listener.sh /usr/local/opt/docker-lifecycle-listener/sbin/
+
+LISTENER_SERVICE_NAME=uk.org.lidalia.docker-lifecycle-listener
+cp $LISTENER_SERVICE_NAME.plist ~/Library/LaunchAgents/
+launchctl load ~/Library/LaunchAgents/$LISTENER_SERVICE_NAME.plist
+launchctl start $LISTENER_SERVICE_NAME
+
+NOTIFIER_NAME=docker-lifecycle-notifier
+
+docker build . -t $NOTIFIER_NAME
+docker rm -f $NOTIFIER_NAME
 docker run \
   --detach \
   --restart always \
-  --name docker-lifecycle-notifier \
-  docker-lifecycle-notifier
+  --name $NOTIFIER_NAME \
+  $NOTIFIER_NAME
